6b3dd2c9efc5f6c5e0bacf92ff970189d6ec03e0
tasddc1226 <tasddc@naver.com>
2021-09-28 12:53:08 +0900

    [fix] listbox 기본 예제 수정, sqlite3 프로젝트 초기 설젱

---
+++ ListBox_SQLite/ListBox_SQLite.h
@@ -0,0 +1,32 @@
+
+// ListBox_SQLite.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CListBox_SQLiteApp:
+// 이 클래스의 구현에 대해서는 ListBox_SQLite.cpp을 참조하십시오.
+//
+
+class CListBox_SQLiteApp : public CWinApp
+{
+public:
+	CListBox_SQLiteApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CListBox_SQLiteApp theApp;
---
+++ ListBox_SQLite/ListBox_SQLiteDlg.h
@@ -0,0 +1,33 @@
+
+// ListBox_SQLiteDlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// CListBox_SQLiteDlg 대화 상자
+class CListBox_SQLiteDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CListBox_SQLiteDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LISTBOX_SQLITE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
